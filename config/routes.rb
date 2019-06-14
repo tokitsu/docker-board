f10b3190ce73d585d6ef52b5167f972eb924b08c
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-06-14 20:08:03 +0900

    init

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'boards', to: 'boards#index'
+    get 'boards/new', to: 'boards#new'
+  root 'boards#index'
+end
