bb94205b8db0da87b5750a226c649f67b6f5a5c3
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-06-15 15:13:07 +0900

    add crud function

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,33 @@
 class BoardsController < ApplicationController
+  def show
+    @board = Board.find(params[:id])
+  end
 
   def index
+    @boards = Board.all
   end
 
   def new
+    @board = Board.new
   end
 
+  def create
+    Board.create(board_params)
+    redirect_to board
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    @board.update(board_params)
+    redirect_to board
+  end
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
 end
