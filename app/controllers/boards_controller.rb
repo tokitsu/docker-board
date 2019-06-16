6517ebd4e622cf08613b70c92b8b25932bb2dc94
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-06-17 04:15:51 +0900

    add association between board and tag

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,12 +1,9 @@
 class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
 
-  def show
-    @comment = @board.comments.new
-  end
-
   def index
-    @boards = Board.page(params[:page])
+    @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
+    @boards = @boards.page(params[:page])
   end
 
   def new
@@ -26,13 +23,24 @@ class BoardsController < ApplicationController
     end
   end
 
+  def show
+    @comment = Comment.new(board_id: @board.id)
+  end
+
   def edit
   end
 
   def update
-    @board.update(board_params)
-    flash[:notice] = "「#{@board.title}」の掲示板を編集しました"
-    redirect_to board_path
+    if @board.update(board_params)
+      flash[:notice] = "「#{@board.title}」の掲示板を編集しました"
+      redirect_to board_path
+    else
+      redirect_to edit_board_path, flash: {
+      board: @board,
+      error_messages: @board.errors.full_messages
+      }
+    end
+
   end
 
   def destroy
@@ -42,7 +50,7 @@ class BoardsController < ApplicationController
   private
 
   def board_params
-    params.require(:board).permit(:name, :title, :body)
+    params.require(:board).permit(:name, :title, :body, tag_ids: [])
   end
 
   def set_target_board
