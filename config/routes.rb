15a4abe80e40969d15f2623a164f22a330ddc29d
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-06-18 02:45:17 +0900

    add login and logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  get 'comments/create'
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
-  get 'comments/destroy'
+  get 'sessions/create'
 
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     #get 'boards', to: 'boards#index'
     #get 'boards/new', to: 'boards#new'
     #post 'boards', to: 'boards#create'
     #get 'boards/:id', to: 'boards#show'
     root 'home#index'
+    resources :users, only: %i[new create]
     resources :boards
     resources :comments
   root 'boards#index'
